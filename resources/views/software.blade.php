e0d323ddc8d7f22f34059ac08e8d979900f86f60
blitzen23 <michaelthe23@gmail.com>
2022-06-21 18:18:30 +0700

    Adding modal for software and course

--- resources/views/software.blade.php
+++ resources/views/software.blade.php
@@ -12,7 +12,21 @@
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
                             additional content. This content is a little bit longer.</p>
-                        <a href="#" class="btn float-right bg-white">Download</a>
+                        <button type="button" href="#" class="btn float-right bg-white" data-toggle="modal" data-target="#download">Download</button>
+                        <div class="modal fade" id="download" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog" role="document" style="background-color: #015791">
+                                <div class="modal-content" style="background-color: #015791">
+                                <div class="modal-body" style="color: white" class="d-flex" style="text-center">
+                                    <div class="text-center h3">Are you sure want to download the file?</div>
+                                </div>
+                                <div class="modal-footer" style="justify-content: center !important">
+                                    <button type="button" class="btn btn-secondary h3" data-dismiss="modal" style="font-weight: bold; margin: 1vw; color: black; background-color: #E8F1F5;">NO</button>
+                                    <button type="button" class="btn btn-primary h3" style="font-weight: bold; margin: 1vw; color: black; background-color: #E8F1F5;">YES</button>
+                                    <div>*After clicking yes, you will be redirected to the link</div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -25,7 +39,21 @@
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
                             additional content. This content is a little bit longer.</p>
-                        <a href="#" class="btn float-right bg-white">Download</a>
+                        <button type="button" href="#" class="btn float-right bg-white" data-toggle="modal" data-target="#download">Download</button>
+                        <div class="modal fade" id="download" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog" role="document" style="background-color: #015791">
+                                <div class="modal-content" style="background-color: #015791">
+                                <div class="modal-body" style="color: white" class="d-flex" style="text-center">
+                                    <div class="text-center h3">Are you sure want to download the file?</div>
+                                </div>
+                                <div class="modal-footer" style="justify-content: center !important">
+                                    <button type="button" class="btn btn-secondary h3" data-dismiss="modal" style="font-weight: bold; margin: 1vw; color: black; background-color: #E8F1F5;">NO</button>
+                                    <button type="button" class="btn btn-primary h3" style="font-weight: bold; margin: 1vw; color: black; background-color: #E8F1F5;">YES</button>
+                                    <div>*After clicking yes, you will be redirected to the link</div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -38,7 +66,21 @@
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
                             additional content. This content is a little bit longer.</p>
-                        <a href="#" class="btn float-right bg-white">Download</a>
+                        <button type="button" href="#" class="btn float-right bg-white" data-toggle="modal" data-target="#download">Download</button>
+                        <div class="modal fade" id="download" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog" role="document" style="background-color: #015791">
+                                <div class="modal-content" style="background-color: #015791">
+                                <div class="modal-body" style="color: white" class="d-flex" style="text-center">
+                                    <div class="text-center h3">Are you sure want to download the file?</div>
+                                </div>
+                                <div class="modal-footer" style="justify-content: center !important">
+                                    <button type="button" class="btn btn-secondary h3" data-dismiss="modal" style="font-weight: bold; margin: 1vw; color: black; background-color: #E8F1F5;">NO</button>
+                                    <button type="button" class="btn btn-primary h3" style="font-weight: bold; margin: 1vw; color: black; background-color: #E8F1F5;">YES</button>
+                                    <div>*After clicking yes, you will be redirected to the link</div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -51,20 +93,21 @@
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
                             additional content. This content is a little bit longer.</p>
-                        <a href="#" class="btn float-right bg-white">Download</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card rounded-3 shadow-lg" style="background-color: #E8F1F5">
-                    <div class="d-flex justify-content-center align-items-center p-3">
-                        <img src="./img/bg.jpg" width="120px" height="120px" alt="...">
-                    </div>
-                    <div class="card-body text-white" style="background-color: #015791">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                        <a href="#" class="btn float-right bg-white">Download</a>
+                        <button type="button" href="#" class="btn float-right bg-white" data-toggle="modal" data-target="#download">Download</button>
+                        <div class="modal fade" id="download" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog" role="document" style="background-color: #015791">
+                                <div class="modal-content" style="background-color: #015791">
+                                <div class="modal-body" style="color: white" class="d-flex" style="text-center">
+                                    <div class="text-center h3">Are you sure want to download the file?</div>
+                                </div>
+                                <div class="modal-footer" style="justify-content: center !important">
+                                    <button type="button" class="btn btn-secondary h3" data-dismiss="modal" style="font-weight: bold; margin: 1vw; color: black; background-color: #E8F1F5;">NO</button>
+                                    <button type="button" class="btn btn-primary h3" style="font-weight: bold; margin: 1vw; color: black; background-color: #E8F1F5;">YES</button>
+                                    <div>*After clicking yes, you will be redirected to the link</div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
